7c329b862451a2a991ea4524543ef6eab835a5c2
Lucas Lima <lucaslimamesmo@gmail.com>
2021-10-02 18:21:24 -0300

    first commit

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
+//Semana 3 no notion
+const express = require('express');
+const nunjucks = require('nunjucks');
 const routes = require('./routes')
-const server = express() 
-const methodOverride = require('method-override') //middleware do override
+const methodOverride = require('method-override')
+const server = express();
 
+server.set('view engine', '.njk');
 
-server.use(express.urlencoded({ extended: true })) //midware para receber os dados no backend
-
-
-server.use(express.static("public")) //observando a pasta public
-server.use(methodOverride('_method')) //deve vir antes do routes para interceptar o routes
+server.use(express.urlencoded({extended:true})) //enables the data to come from the front-end
+server.use(express.static('public'));
+server.use(methodOverride('_method')) // this override needs to be placed before the routes usage
 server.use(routes)
 
-server.set("view engine","njk")
 
-nunjucks.configure("views",{
-    express:server,
+nunjucks.configure("views", {
+    express: server,
     autoescape:false,
     noCache: true
+});
+
+server.listen(5000, function() {
+    console.log("My server is running. . .")
 })
-
-server.listen(5000,function(){
-    console.log("O servidor ta rodando por enqto.....")
-})
-
-
