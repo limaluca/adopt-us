c6a088a52b62d89a9e5bd837c14450f7ff3981ef
Lucas Lima <lucaslimamesmo@gmail.com>
2021-10-03 18:01:55 -0300

    addied the selected menu styling

--- public/scripts.js
+++ public/scripts.js
@@ -1,29 +1,11 @@
-// selecting an object (a class) called .modalOverlay
-const modalOverlay = document.querySelector('.modalOverlay');
-const modal = document.querySelector('.modal');
+const currentLocation = location.pathname
 
-// selecting every object named .card using querySelectorALL
-const cards = document.querySelectorAll('.card');
+const navigationAnchors = document.querySelectorAll("#nav");
 
+console.log(currentLocation)
+if(currentLocation=='/dogs'){
+    navigationAnchors[0].style.fontWeight = 'bold'
+} else {
+    navigationAnchors[1].style.fontWeight = 'bold'
 
-for (let card of cards) {
-    card.addEventListener('click', function() {
-        const pageId = card.getAttribute("id");
-        window.location.href = `/course?id=${pageId}`
-
-
-    })
 }
-
-
-
-
-
-
-// document.querySelector('.maximizeModal').addEventListener('click', function() {
-//     if (modal.classList.contains('maximize')) {
-//         modal.classList.remove('maximize')
-//     } else {
-//         modal.classList.add('maximize')
-//     }
-// })
